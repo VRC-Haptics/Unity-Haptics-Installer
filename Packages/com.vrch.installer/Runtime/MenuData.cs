e98d8a8dded5ebed885c191b409df84495a15a60
VirtuallyAverage <virtuallyaverage@gmail.com>
2025-11-15 10:12:52 -0600

    Set runtime data constructors into IEditorOnly to remove "auto fix" error

--- Packages/com.vrch.installer/Runtime/MenuData.cs
+++ Packages/com.vrch.installer/Runtime/MenuData.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using VRC.SDK3.Avatars.ScriptableObjects;
+using VRC.SDKBase;
 
 namespace HapticsInstaller.Runtime
 {
@@ -8,7 +9,7 @@ namespace HapticsInstaller.Runtime
     /// Small data class to hold the target bone of the Menu. Since we can't pull data from VRCFury assets.
     /// </summary>
     [Serializable]
-    public class MenuData : MonoBehaviour
+    public class MenuData : MonoBehaviour, IEditorOnly
     {
         /// <summary>
         /// The Menu for This Prefab that needs to be merged
--- Packages/com.vrch.installer/Runtime/TargetBone.cs
+++ Packages/com.vrch.installer/Runtime/TargetBone.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
+using VRC.SDKBase;
 
 namespace HapticsInstaller.Runtime
 {
     /// <summary>
     /// Small data class to hold the target bone of each Node. Since we can't pull data from VRCFury assets.
     /// </summary>
-    public class TargetBone : MonoBehaviour
+    public class TargetBone : MonoBehaviour, IEditorOnly
     {
         public HumanBodyBones targetBone;
     }
